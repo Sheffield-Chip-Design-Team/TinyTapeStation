5fa1a47e6a4f51f0534476adcc9682234724a777
Kitsudora <bshi12@sheffield.ac.uk>
2025-11-09 13:59:41 +0000

    Update APUTrigger.v

--- src/APUTrigger.v
+++ src/APUTrigger.v
@@ -18,28 +18,28 @@ module APU_trigger (
     if (reset) begin
       trigger_buf <= 3'b0;
     end else begin
-      if (frame_end) begin
+      // if (frame_end) begin
         trigger_buf[0] <= SheepDragonCollision;
         trigger_buf[1] <= SwordDragonCollision;
         trigger_buf[2] <= PlayerDragonCollision;
-      end
+      // end
     end
   end
   
   // detect the rising edge of the trigger signals
   always @ (posedge clk) begin
     if (!test_mode) begin
-      if (trigger_buf[0] & ~trigger_buf[0])  
+      if (SheepDragonCollision & ~trigger_buf[0])  
         eat_sound <= 1'b1;
       else 
         eat_sound <= 1'b0;
 
-      if (~trigger_buf[1] & trigger_buf[1])  
+      if (SwordDragonCollision & ~trigger_buf[1])  
         die_sound <= 1'b1;
       else 
         die_sound <= 1'b0;
 
-      if (~trigger_buf[2] & trigger_buf[2])  
+      if (PlayerDragonCollision & ~trigger_buf[2])  
         hit_sound <= 1'b1;
       else 
         hit_sound <= 1'b0;
